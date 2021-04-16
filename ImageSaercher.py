abc2e46e851ba33c85de4bc2be9a641963002ee4
@kn2282 <k7668n.kk@gmail.com>
2021-04-16 17:49:38 +0200

    update ImageSaercher.py

--- ImageSaercher.py
+++ ImageSaercher.py
@@ -1,14 +1,34 @@
 from pip._internal.utils.misc import enum
 
 class AlgorithmType(enum):
-    smallImages = 1
-    bigImages = 2
-    #TODO
+    HALF_IMAGES = 1
+    QUARTER_IMAGES = 2
+    HORIZONTAL = 3
+    VERTICAL = 4
+
 
 class SearchAlgorithm:
-    def __init__(self, type: AlgorithmType, img_proportions: float = 1):
-        #TODO
-        pass
+    def __init__(self, type: AlgorithmType, img_prop: float = 1, model_input_prop: float = 1, max_depth: int = 4):
+        """
+
+        :type max_depth: max depth of saerchimg inside picture.
+         Bigger depth -> algorithm searches to smaller pice of picture
+        """
+        self.max_depth = max_depth
+        self.model_input_prop = model_input_prop
+        self.type = type
+        self.img_prop = img_prop
+
+        if img_prop > model_input_prop:
+            self.__main_y_prop = 1.
+            self.__main_x_prop = model_input_prop / img_prop
+        else:
+            self.__main_x_prop = 1.
+            self.__main_y_prop = img_prop / model_input_prop
+
+        if type == AlgorithmType.HALF_IMAGES:
+            self.__no_pictures_in_depth = [ 2**i for i in range(1, max_depth+1)]
+            self.__prop_divider_func = lambda x: 2/(x + 1)
 
 class ImageSearcher:
     def __init__(self, model, algorithm: SearchAlgorithm):
